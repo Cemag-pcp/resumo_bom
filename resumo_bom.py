e3032445d50e07244be2c9b98cc1e236846eedb8
Cemag-pcp <109666229+Cemag-pcp@users.noreply.github.com>
2022-07-28 09:40:30 -0300

    Update resumo_bom.py

--- resumo_bom.py
+++ resumo_bom.py
@@ -7,14 +7,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-features=NetworkService")
-options.add_argument("--window-size=1920x1080")
-options.add_argument("--disable-features=VizDisplayCompositor")
+#options = Options()
+#options.add_argument("--headless")
+#options.add_argument("--no-sandbox")
+#options.add_argument("--disable-dev-shm-usage")
+#options.add_argument("--disable-gpu")
+#options.add_argument("--disable-features=NetworkService")
+#options.add_argument("--window-size=1920x1080")
+#options.add_argument("--disable-features=VizDisplayCompositor")
 
 
 def delete_selenium_log():
@@ -37,7 +37,7 @@ def show_selenium_log():
 
 def run_selenium():
     name = str()
-    with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
+    with webdriver.Chrome(service_log_path='selenium.log') as driver:
         url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
         driver.get(url)
         xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
