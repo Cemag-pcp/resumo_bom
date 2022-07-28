dad7e1f023e5511ddbaa21bbdd866ef6506c4609
Cemag-pcp <109666229+Cemag-pcp@users.noreply.github.com>
2022-07-28 09:45:56 -0300

    Update resumo_bom.py

--- resumo_bom.py
+++ resumo_bom.py
@@ -4,6 +4,7 @@ import gspread
 import numpy as np
 import glob
 import os
+import streamlit as st
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,6 +13,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #from gspread_dataframe import set_with_dataframe
 from selenium.webdriver.chrome.options import Options
+
 
 options = Options()
 options.add_argument("--headless")
